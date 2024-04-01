4d54efeec7dbdd1df99a78a075965cdbdb6c5cef
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-04-01 08:41:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001443337704"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001970031336"))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/MAHTO-ANJALI/AnjaliMusicOne",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_CREATORS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_FRIENDSS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ANJALIxWORLD")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AnjalixSupportxGroup")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
@@ -90,12 +90,12 @@ STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
 
-#    __      _______ _____    ___  __ _    _  _____ _____ _____   _____   ____ _______ 
-#    \ \    / /_   _|  __ \   |  \/  | |  | |/ ____|_   _/ ____|  |  _ \ / __ \__   __|
-#     \ \  / /  | | | |__) |  | \  / | |  | | (___   | || |       | |_) | |  | | | |   
-#      \ \/ /   | | |  ___/   | |\/| | |  | |\___ \  | || |       |  _ <| |  | | | |   
-#       \  /   _| |_| |       | |  | | |__| |____) |_| || |____   | |_) | |__| | | |   
-#        \/   |_____|_|       |_|  |_|\____/|_____/|_____\_____|  |____/ \____/  |_|   
+#      _    _   _     _   _    _     ___      __  __ _   _ ____ ___ ____   
+#     / \  | \ | |   | | / \  | |   |_ _|    |  \/  | | | / ___|_ _/ ___|  
+#    / _ \ |  \| |_  | |/ _ \ | |    | |     | |\/| | | | \___ \| | |      
+#   / ___ \| |\  | |_| / ___ \| |___ | |     | |  | | |_| |___) | | |___   
+#  /_/   \_\_| \_|\___/_/   \_\_____|___|    |_|  |_|\___/|____/___\____|  
+#                                                                          
 
 
 
@@ -113,21 +113,21 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/529708a1552ff52d2d362.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/68951be447d8a16eabc00.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4b394adb26a12344e28de.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/9b4b059f9275da973852f.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/72978323f2133f3e5b345.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/e4a87f829cfa4634f8ca1.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/b8c28591a6eb45d8ef9d3.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/0bc5168ac53c4301580a4.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/4666f33d82fa43338ab71.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/8c8b21edb967f42f4fc3a.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/9a4bb797fceb59c0428b6.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/1e05c61da6c394e6eb293.jpg"
 
 
 def time_to_seconds(time):
